964c7b85de99d320881371c58d0c6696d875053d
lvrossem <lukas.vanrossem1@gmail.com>
2021-02-05 01:24:13 +0100

    Change key mapping to be more suited for AZERTY

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,22 @@
 
 // Keypad keymap
 uint8_t keymap[16] = {
-    SDLK_x,
     SDLK_1,
     SDLK_2,
     SDLK_3,
-    SDLK_q,
-    SDLK_w,
-    SDLK_e,
+    SDLK_4,
     SDLK_a,
+    SDLK_z,
+    SDLK_e,
+    SDLK_r,
+    SDLK_q,
     SDLK_s,
     SDLK_d,
-    SDLK_z,
-    SDLK_c,
-    SDLK_4,
-    SDLK_r,
     SDLK_f,
-    SDLK_v,
+    SDLK_w,
+    SDLK_x,
+    SDLK_c,
+    SDLK_v
 };
 
 int main(int argc, char **argv) {
